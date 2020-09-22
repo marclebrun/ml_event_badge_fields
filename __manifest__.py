a6e27ab9c4fb906ddf70fc1de7f815635a01376f
Marc Lebrun <marclebrun2@gmail.com>
2020-09-22 06:53:01 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ML Event Badge Fields',
+    'version': '0.1',
+    'author': 'Marc Lebrun',
+    'description': 'Adds editing of text fields on badge front/back/left/right.',
+    'installable': True,
+    'application': False,
+    'depends': [
+        'event',
+    ],
+    'data': [
+        'views/forms.xml',
+    ]
+}
